170a98863464a3d669c1759bf47f7af0c54fd5b3
AndrMoura <andre_ptos29@msn.com>
2023-11-06 13:43:57 +0000

    Fix formatting and error handling in makePostRequest function

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ async function setupPlugin({ config }) {
 }
 
 async function makePostRequest(url, data) {
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept-Encoding": "gzip, deflate, br",
-          "Connection": "keep-alive",
-          "Accept": "*/*",
-           
-        },
-        body: JSON.stringify(data)
-      });
-  
-      if (response.status === 200) {
-        const responseData = await response.json();
-        return responseData;
-      } else {
-        console.error("Request message " + response);
-      }
-    } catch (error) {
-      console.error("Error:", error);
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Accept": "*/*",
+         
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (response.status === 200) {
+      const responseData = await response.json();
+      return responseData;
+    } else {
+      console.error("Request code " + response.status);
     }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 async function processEvent(event, { config, cache }) {
